be36900a90667af48dc78a85de76d46150245b4d
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-03-16 00:10:49 +0400

    add tests

---
+++ tests/test_gendiff.py
@@ -0,0 +1,17 @@
+from gendiff.scripts import gendiff
+from pathlib import Path
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+def test_gendiff():
+    file1 = gendiff.parse_json(get_test_data_path("first.json"))
+    file2 = gendiff.parse_json(get_test_data_path("second.json"))
+    expected = read_file("expected.txt")
+    actual = gendiff.generate_diff(file1, file2)
+    assert actual == expected
+
+
