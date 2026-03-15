d46fa78d180cf83c4adf5b0ecd59a13d1566ea25
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-03-16 01:00:00 +0400

    make fix

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,19 @@
 from gendiff.scripts import gendiff
 from pathlib import Path
 
+
 def get_test_data_path(filename):
+
     return Path(__file__).parent / "test_data" / filename
 
+
 def read_file(filename):
+
     return get_test_data_path(filename).read_text()
 
+
 def test_gendiff():
+
     file1 = gendiff.parse_json(get_test_data_path("first.json"))
     file2 = gendiff.parse_json(get_test_data_path("second.json"))
     expected = read_file("expected.txt")
